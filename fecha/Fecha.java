6d305bf08807d0340119c978bded5b9719f54263
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-04-02 15:52:40 +0000

    nuevos metodos en clase Fecha

--- fecha/Fecha.java
+++ fecha/Fecha.java
@@ -35,6 +35,49 @@ public class Fecha{
          this.anio = Integer.parseInt(sAnio);
 
     }
+    //Metodos personalizados
+    //suma dias a una Fecga
+    public void addDias(int d){
+        //convertir la fecha a dias y luego sumarle d
+        int sum = fechaToDias()+d;
+
+        //la fecha resultante (sum) se separa en dia,mes y año
+        diasToFecha(sum);
+
+    }
+    private int fechaToDias(){
+        return anio*360+mes*30+dia;
+    }
+    private void diasToFecha(int i){
+        //duvidimos por 360 y obtenemos el año
+       this.anio = (int)i/360;
+        //del resto o residuo de la division anterior
+        //podremos obtener el mes y el dia
+        int resto = i % 360;
+
+        //el mes es el resto dividido entre 30
+       this.mes = (int) resto/30;
+
+        //el resto de la division anterior son los dias
+
+        this.dia = resto % 30;
+
+        //ajuste por si el dia quedo en cero
+        if (dia ==0){
+            dia = 30;
+            mes--;
+        }
+
+        //ajuste por el mes quedo en cero
+        if (mes ==0){
+            mes = 12;
+            anio--;
+        }
+
+
+
+
+    }
     //getter y setter
     public int getDia(){
         return this.dia;
